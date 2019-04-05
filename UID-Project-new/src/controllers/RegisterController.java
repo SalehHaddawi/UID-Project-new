3620a2c3263d4299def85f3b6417e44662063b06
saleh haddawi <saleh.haddawi@gmail.com>
2019-04-05 22:18:36 +0300

    Add files via upload

---
+++ UID-Project-new/src/controllers/RegisterController.java
@@ -0,0 +1,56 @@
+package controllers;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import lunch.Lunch;
+
+/**
+ * FXML Controller class
+ *
+ * @author عبوديالغامدي
+ */
+public class RegisterController implements Initializable {
+
+    @FXML
+    private TextField fullNameTextFiled;
+    @FXML
+    private TextField emailTextFiled;
+    @FXML
+    private PasswordField passwordFiled;
+    @FXML
+    private PasswordField confirmPasswordFiled;
+
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+
+    @FXML
+    private void onClose(ActionEvent event) {
+        System.exit(0);
+    }
+
+    @FXML
+    private void goToLogin(MouseEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/views/Login.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        Lunch.appStage.setScene(scene);
+    }
+
+    @FXML
+    private void register(ActionEvent event) {
+    }
+}
