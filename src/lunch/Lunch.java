fc794fc60d8c4a56924e0d77f4ae19dfb3f2ab4a
saleh haddawi <saleh.haddawi@gmail.com>
2019-04-08 07:53:41 +0300

    made the stage resizable

--- src/lunch/Lunch.java
+++ src/lunch/Lunch.java
@@ -1,34 +1,39 @@
 package lunch;
 
+import utils.ResizeHelper;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.PopupControl;
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class Lunch extends Application {
-    
+
     public static Stage appStage;
-        
+
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/views/Login.fxml"));
-        
+
         Scene scene = new Scene(root);
-        
+
         stage.setScene(scene);
-        
+
         stage.initStyle(StageStyle.TRANSPARENT);
-        
+
         stage.getScene().setFill(Color.TRANSPARENT);
-        
+
         appStage = stage;
-                
+
         stage.show();
+
+        ResizeHelper.addResizeListener(stage, 1000, 600, Double.MAX_VALUE, Double.MAX_VALUE);
     }
-    
+
     public static void main(String[] args) {
         launch(args);
     }
